53ed9793fe5b98a766ae35f6236257e5495cdad4
Hrudai Maduri <hrudaimaduri07@gmail.com>
2025-12-11 17:13:40 +0530

    Added Separationof concerns in model and createa a owner model as well

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
- 
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require("./config/MongooseConncetion");
+const OwnerRouter = require("./routes/OwnerRouter");
+const ProductRouter = require("./routes/ProductRouter");
+const UserRouter = require("./routes/UserRouter");
+
+
+
+app.set("view engine" , "ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(cookieParser());
+
+app.get("/",function(req,res){
+    res.send("hey");
+
+})
+app.use("/owner",OwnerRouter);
+app.use("/user",UserRouter);
+app.use("/product",ProductRouter);
+
+
+
+
+app.listen(3030);
